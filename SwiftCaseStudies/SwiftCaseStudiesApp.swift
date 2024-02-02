841daf28d8b6f8eb7c8e325d5a1e54bbce26e8f6
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2024-02-02 16:35:35 +0200

    Initial Commit

---
+++ SwiftCaseStudies/SwiftCaseStudiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCaseStudiesApp.swift
+//  SwiftCaseStudies
+//
+//  Created by Telem Tobi on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCaseStudiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
